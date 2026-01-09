0d1fe6ba199090b9f664f3c95b8e7801499acd21
nulqwy <ivan.demian2009@gmail.com>
2026-01-09 20:09:48 +0200

    build the site

--- src/lib/text/Text.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	// import type { Snippet } from 'svelte';
-
-	let { children }: { children: any } = $props();
-</script>
-
-<p class="text-text dark:text-text-dark">
-	{@render children()}
-</p>
